c0b923d9b682bcda7d54a636690f653e864c35b6
JoanMz <joanmzteo@gmail.com>
2024-08-30 22:00:00 -0500

    update

--- Sql_connection/postgres_connect.py
+++ Sql_connection/postgres_connect.py
@@ -1,53 +1,18 @@
 from decouple import config
 from datetime import datetime
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String, DateTime
-from sqlalchemy.orm import sessionmaker, declarative_base
+from logging import Logger
 
-engine = create_engine(f"postgresql://{config("DB_USER")}:{config("DB_PASSWORD")}@{config("DB_HOST")}/{config("DB_DATABASE")}")
-base = declarative_base()
+logger = Logger(name='log', level=20)
 
-class precleaned_table(base):
-    __tablename__ = config("DB_TABLE1")
-    id = Column(Integer(), primary_key=True)
-    First_name = Column(String(30))
-    Last_name = Column(String(30))
-    Email = Column(String(50))
-    Application_date = Column(DateTime())
-    Country = Column(String(50))
-    YOE = Column(Integer())
-    Seniority = Column(String(30))
-    Code_challenge_score = Column(Integer())
-    Technical_Interview_Score = Column(Integer())
+def connection():
+    try:
+        engine = create_engine(f"postgresql://{config("DB_USER")}:{config("DB_PASSWORD")}@{config("DB_HOST")}/{config("DB_DATABASE")}")
+        logger.log(level=20, msg=f'DB_ENGINE Charge {datetime.now()}')
+        return engine
+    except:
+        logger.log(level=40, msg='An error ocurre')
+        return None
+        
 
-    def __str__(self):
-        return self.username
-
-class hires_table(base):
-    __tablename__= config("DB_TABLE2")
-    id = Column(Integer(), primary_key=True)
-    First_name = Column(String(30))
-    Last_name = Column(String(30))
-    Email = Column(String(50))
-    Application_date = Column(DateTime())
-    Country = Column(String(50))
-    YOE = Column(Integer())
-    Seniority = Column(String(30))
-    Code_challenge_score = Column(Integer())
-    Technical_Interview_Score = Column(Integer())
-    Hired = Column(Integer())
-
-    def __str__(self):
-        return self.username
-
-
-Session = sessionmaker(engine)
-session = Session()
-
-def connection(engine=engine):
-    return engine
-
-
-if __name__ == "__main__":
-    base.metadata.drop_all(engine)
-    base.metadata.create_all(engine)
+    
